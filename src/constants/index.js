d906c7205c55e32f7862caff514d4333eaddae34
kartikeya Sharma <karthiksharma1411@gmail.com>
2021-10-18 10:47:51 +0530

    first commi in web developlment video call

---
+++ src/constants/index.js
@@ -0,0 +1,10 @@
+export const rtc = {
+    client: null,
+    localAudioTrack: null,
+    localVideoTrack: null,
+  };
+  
+  export const options = {
+    appId: "856ef62afbe74890b47456cf5230953f",
+    token: null,
+  };
